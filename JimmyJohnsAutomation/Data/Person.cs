6c281ef1d9cd2853c8cee437f68133810611c240
QAAutomationRules <avzrule@gmail.com>
2017-07-31 14:55:15 -0600

    add phone scenario

--- JimmyJohnsAutomation/Data/Person.cs
+++ JimmyJohnsAutomation/Data/Person.cs
@@ -11,5 +11,11 @@ namespace JimmyJohnsAutomation.Data
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+
+    }
+
+    public class PhoneData
+    {
+        public string Phone { get; set; }
     }
 }
---
+++ JimmyJohnsAutomation/Steps/PhoneContextInjection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JimmyJohnsAutomation.Data;
+using JimmyJohnsAutomation.Pages;
+using JimmyJohnsAutomation.WebDriverExtensions;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace JimmyJohnsAutomation.Steps
+{
+    [Binding]
+    public sealed class PhoneContextInjection
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>("IWebDriver");
+
+        private readonly PhoneData phoneData;
+
+        public PhoneContextInjection(PhoneData phoneData)
+        {
+            this.phoneData = phoneData;
+        }
+
+        [Given(@"I have a phone number ""(.*)""")]
+        public void GivenIHaveAPhoneNumber(string phoneNumber)
+        {
+            phoneData.Phone = phoneNumber;
+        }
+
+        [Then(@"the phone number is added to the phone number field")]
+        public void ThenThePhoneNumberIsAddedToThePhoneNumberField()
+        {
+            CreateAccountPage createAccountPage = new CreateAccountPage(driver);
+            createAccountPage.PhoneNumberTextBox.SetText(phoneData.Phone);
+        }
+
+    }
+}
