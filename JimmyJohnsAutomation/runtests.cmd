2b84fb229634d70b84bbbaf7ffe9278cd238f382
QAAutomationRules <avzrule@gmail.com>
2017-08-09 12:22:39 -0600

    add slow cheetah

--- JimmyJohnsAutomation/runtests.cmd
+++ JimmyJohnsAutomation/runtests.cmd
@@ -1,6 +1,6 @@
 @pushd %~dp0
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "JimmyJohnsAutomation.csproj"
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "JimmyJohnsAutomation.csproj" /p:Pickles_Generate=True /p:Pickles_FeatureDirectory=C:\jimmy\JimmyJohnsAutomation\JimmyJohnsAutomation\Features /p:Pickles_DocumentationFormat=word /p:Pickles_OutputDirectory=C:\MyFeatures
 
 @if ERRORLEVEL 1 goto end
 
